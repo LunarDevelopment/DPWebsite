ccef71f6f6dcb6da350c5be87fe4cc41be3c40c4
Benjamin <jackson-ben@outlook.com>
2016-11-22 17:06:35 +0000

    version 1.1.1

--- app/Http/Controllers/Frontend/Website/SiteMapController.php
+++ app/Http/Controllers/Frontend/Website/SiteMapController.php
@@ -3,30 +3,30 @@
 namespace App\Http\Controllers\Frontend\Website;
 
 use App\Http\Controllers\Controller;
-use Sitemap;
+//use Sitemap;
 
 class SiteMapController extends Controller
 {
     public function __construct()
     {
-        \Debugbar::disable();
+//        \Debugbar::disable();
     }
 
     public function index()
     {
-        return view('frontend.sitemap');
+//        return view('frontend.sitemap');
     }
 
     public function scan()
     {
-        // Get a general sitemap.
-        Sitemap::addSitemap('/sitemaps/general');
-
-        // You can use the route helpers too.
-        Sitemap::addSitemap(URL::route('sitemaps.posts'));
-        Sitemap::addSitemap(route('sitemaps.users'));
-
-        // Return the sitemap to the client.
-        return Sitemap::index();
+//        // Get a general sitemap.
+//        Sitemap::addSitemap('/sitemaps/general');
+//
+//        // You can use the route helpers too.
+//        Sitemap::addSitemap(URL::route('sitemaps.posts'));
+//        Sitemap::addSitemap(route('sitemaps.users'));
+//
+//        // Return the sitemap to the client.
+//        return Sitemap::index();
     }
 }
